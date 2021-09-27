5747202d361e52f17624034a45045cf5956dccac
Javier Correa <javiercorrear@gmail.com>
2021-09-27 10:56:49 -0300

    feat(public): implement interface which enables event testing.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require( 'express' )
+const path = require( 'path' )
+const ejs = require( 'ejs' )
 
 const app = express()
 const server = require( 'http' ).createServer( app )
@@ -10,8 +12,19 @@ const io = require( 'socket.io' )( server, {
   allowEIO3: true
 } )
 
+process.env.EVENT_NAME = 'default'
 const PORT = process.env.PORT || 3000
 
+app.use( express.static( path.join( __dirname, 'public' ) ) )
+app.set( 'views', path.join( __dirname, 'public' ) )
+app.engine( 'html', ejs.renderFile )
+app.set( 'view engine', 'html' )
+
+app.use( '/', ( req, res ) => {
+  res.render( 'index.html' )
+} )
+
+// TO DO: make sure the room is removed after all participants have left the room
 const ROOMS = {}
 
 const generateId = ( () => {
@@ -35,6 +48,17 @@ io.on( 'connection', socket => {
       return socket.join( roomName )
     }
     return socket.emit( 'error', JSON.stringify( { message: 'The room you are trying to access doesn\'t exists' } ) )
+  } )
+
+  socket.on( 'test', data => {
+    console.log( data )
+    try {
+      const content = JSON.stringify( data.content )
+      socket.emit( 'receivedMessage', { ...data, content } )
+    } catch ( error ) {
+      console.error( error.stack )
+      socket.emit( 'receivedMessage', { name: data.name, content: error.message } )
+    }
   } )
 
   socket.on( 'leave_room', ( roomName ) => {
